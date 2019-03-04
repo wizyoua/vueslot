1e844cde455cee372d5e00803cbea0269a965ee4
Gianni Zamora <gianni@Giannis-MacBook-Pro.local>
2019-03-04 10:53:14 -0700

    first commit

---
+++ src/components/formOne.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <form-helper>
+            <div slot="form-header">
+                <header-one />
+            </div>
+            <div slot="form-fields">
+                <input type="text" placeholder="name" required />
+                <label>Your Message:</label>
+                <textarea></textarea>
+            </div>
+            <div slot="form-controls">
+                <button v-on:click="handleSubmit">Send</button>
+            </div>
+        </form-helper>
+    </div>
+</template>
+
+<script>
+// Imports
+import formHelper from './formHelper.vue'
+import HeaderOne from './headerFormOne.vue'
+
+export default {
+    components: {
+        'form-helper': formHelper,
+        'header-one': HeaderOne
+
+    },
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+        handleSubmit: function(){
+            alert('thanks for submitting form one & contacting us');
+        }
+    }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+</style>
---
+++ src/components/headerFormOne.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h3>Form One test - Contact Us coming from</h3>
+        <p>Fill in this form to contact us</p>
+    </div>
+</template>
+
